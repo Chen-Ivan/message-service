3d28be73a68ca1b8ac5b3f3e20aeef518552ef8d
ivan <805632731@qq.com>
2021-10-19 22:18:27 +0800

    init message-service

---
+++ src/main/java/com/ivan/message/MessageApplication.java
@@ -0,0 +1,19 @@
+package com.ivan.message;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>消息启动类</p>
+ *
+ * @author ivan
+ * @className MessageApplication
+ * @since 2021/10/17 11:39
+ */
+@SpringBootApplication
+public class MessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
+}
